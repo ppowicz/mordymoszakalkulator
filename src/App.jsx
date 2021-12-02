4de45cf69f92f5830bd443b45db17c5215e7f53f
Patryk Popowicz <ptrykppowicz@gmail.com>
2021-12-02 16:39:52 +0100

    update calc add reverse

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react"
 
 export default function App() {
-  const [input, setInput] = useState(500)
-  const [output, setOutput] = useState("")
+  const rate = 90
 
   const convert = number => {
-    return number / 90
+    return number / rate
   }
+
+  const convertReverse = number => {
+    return number * rate
+  }
+
+  const [input, setInput] = useState()
+  const [output, setOutput] = useState()
+  const [inputReverse, setInputReverse] = useState()
+  const [outputReverse, setOutputReverse] = useState()
+
 
   const onChange = e => {
     setInput(e.target.value)
@@ -18,17 +27,37 @@ export default function App() {
     setOutput(conversion)
   }
 
+  const onChangeReverse = e => {
+    setInputReverse(e.target.value)
+    let conversion = 0
+    try {
+      conversion = convertReverse(e.target.value)
+    } catch {}
+
+    setOutputReverse(conversion)
+  }
+
   return (
     <div className="center">
       <h1>Kalkulator mord moszeknopowskich</h1>
       <div className="form">
         <div className="field">
-          <input className="input" value={input} type="number" onChange={onChange} />
+          <input className="input" defaultValue="500" value={input} onChange={onChange} />
           ml
         </div>
         <div className="field margin">
           <input className="output" value={output} readOnly />
           mM
+        </div>
+      </div>
+      <div className="form">
+        <div className="field margin-reverse">
+          <input className="inputReverse" defaultValue="10" value={inputReverse} onChange={onChangeReverse} />
+          mM
+        </div>
+        <div className="field">
+          <input className="outputReverse" value={outputReverse} readOnly />
+          ml
         </div>
       </div>
     </div>
